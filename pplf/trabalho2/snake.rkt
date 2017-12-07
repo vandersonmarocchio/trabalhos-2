54e5f67e82926b728656674efe1041838a4f5c0d
diogoaalmeida <diogoada@gmail.com>
2017-12-07 15:32:25 -0200

    ...

--- pplf/trabalho2/snake.rkt
+++ pplf/trabalho2/snake.rkt
@@ -1,23 +1,23 @@
 #lang racket
 (require racket/gui)
 
-; tamanho da altura e da largura, em blocos
-(define BLOCKS_WIDTH 30)
-(define BLOCKS_HEIGHT 30)
+; tamanho da altura e da largura do jogo
+(define BLOCKS_WIDTH 20)
+(define BLOCKS_HEIGHT 20)
 
 ; tamanho em pixels de cada bloco
 (define BLOCK_SIZE 16)
 
-; lista com as posições da cobra
-(define cobra (list (list 2 1) (list 1 1)))
+; lista com a posição inicial da cobra
+(define cobra (list (list 10 0) (list 11 0)))
 
-; para onde a cobra está indo
-(define direcao 'r)
+; diração para onde a cobra está indo
+(define direcao 'direita)
 
-; onde a pepsi está
-(define pepsi (list 9 8))
+; onde a comida está
+(define comida (list 9 8))
 
-; quantas pepsis tomou
+; quantas comidas comeu
 (define pontos 0)
 
 ; define a melhor pontuação
@@ -28,72 +28,88 @@
   (cond
     [(> pontos recorde) pontos]
     [else recorde]))
-  
-;define IA
-(define IA #t)
+
+; define IA como verdadeira
+(define IA #f)
+
+; anda sozinho
+; ação: verifica a distancia entre a cobra e a comida, em seguida calcula o valor absoluto de x e y.
+(define (andando-sozinho)
+  (define x  (- (pega-cabeca 0 cobra) (list-ref comida 0)))
+  (define y  (- (pega-cabeca 1 cobra) (list-ref comida 1)))
+  (define abs-x (abs x))
+  (define abs-y (abs y))
+  (cond
+   [(> abs-x abs-y)
+    (if (> x 0) (if (eq? direcao 'direita) 'cima 'esquerda)
+                (if (eq? direcao 'esquerda) 'baixo 'direita))]
+   [else
+    (if (> y 0) (if (eq? direcao 'baixo) 'esquerda 'cima)
+                (if (eq? direcao 'cima) 'direita 'baixo))]))
+
 
 ; move-bloco: inteiro inteiro -> lista
-; objetivo: remove o último elemento da lista e adiciona um novo no começo dela
-(define (move-bloco x y) 
+; ação: remove o último elemento da lista e adiciona um novo no começo dela
+(define (move-bloco x y)
   (reverse (append (cdr (reverse cobra)) (list(list x y)))))
 
 ; pega-cabeca: inteiro lista -> ?
-; objetivo: pega o elemento no indice do arugmento posicao do primeiro elemento da lista
+; ação: pega o elemento no indice do arugmento posicao do primeiro elemento da lista
 ; exemplo:  (pega-cabeca 1 (list (list 4 5) (list 6 7))) -> 4
 (define (pega-cabeca posicao lst)
   (list-ref (list-ref lst 0) posicao))
 
 ; draw-block: dc inteiro inteiro string -> void
-; objetivo: desenha um bloco no objeto de desenho dado
+; ação: desenha um bloco no objeto de desenho dado
 ;           o argumento color pode ser uma string ou um objeto de cor
-(define (draw-block screen x y color) 
+(define (draw-block screen x y color)
   (send screen set-brush color 'solid)
   (send screen draw-rectangle (* x BLOCK_SIZE) (* y BLOCK_SIZE) BLOCK_SIZE BLOCK_SIZE))
 
 ; move-cobra: simbolo -> void
-; objetivo: move a cobra para posição dada('l, 'r, 'u ou 'd)
+; ação: move a cobra para posição dada('esquerda, 'direita, 'cima ou 'baixo)
 (define (move-cobra posicao)
   (case posicao
-    ['l (set! cobra (move-bloco (- (pega-cabeca 0 cobra) 1) (pega-cabeca 1 cobra)))]
-    ['r (set! cobra (move-bloco (+ (pega-cabeca 0 cobra) 1) (pega-cabeca 1 cobra)))]
-    ['u (set! cobra (move-bloco (pega-cabeca 0 cobra) (- (pega-cabeca 1 cobra) 1)))]
-    ['d (set! cobra (move-bloco (pega-cabeca 0 cobra) (+ (pega-cabeca 1 cobra) 1)))]))
+    ['esquerda (set! cobra (move-bloco (- (pega-cabeca 0 cobra) 1) (pega-cabeca 1 cobra)))]
+    ['direita (set! cobra (move-bloco (+ (pega-cabeca 0 cobra) 1) (pega-cabeca 1 cobra)))]
+    ['cima (set! cobra (move-bloco (pega-cabeca 0 cobra) (- (pega-cabeca 1 cobra) 1)))]
+    ['baixo (set! cobra (move-bloco (pega-cabeca 0 cobra) (+ (pega-cabeca 1 cobra) 1)))]))
 
 ; encostou-bloco: lista lista [número] [número] -> bool
-; objetivo: verifica se a lista de pontos pepsi está na lista cobra
+; ação: verifica se a lista de pontos comida está na lista cobra
 ;           g é o índice de exclusão
-(define (encostou-bloco cobra bloco [i 0] [g 666]) 
+(define (encostou-bloco cobra bloco [i 0] [g 666])
   ;(displayln g)
   (if (> (length cobra) i)  ; length (cobra) > i
-    (if (and (not (= g i)) (and 
+    (if (and (not (= g i)) (and
       (eq? (list-ref (list-ref cobra i) 0) (list-ref bloco 0)) ; verifica pelo x e pelo y
-      (eq? (list-ref (list-ref cobra i) 1) (list-ref bloco 1)))) 
+      (eq? (list-ref (list-ref cobra i) 1) (list-ref bloco 1))))
         #t
       (encostou-bloco cobra bloco (+ i 1) g))
     #f))
 
 ; cresce-cobra: -> void
-; objetivo: faz a cobra crescer, copiando o último elemento da lista cobra, movendo a cobra para outra posiçao
+; ação: faz a cobra crescer, copiando o último elemento da lista cobra, movendo a cobra para outra posiçao
 ;           e adicionando o último elemento pego anteriormente
-(define cresce-cobra (lambda () 
+(define cresce-cobra (lambda ()
   (define x (car (reverse cobra)))
-  (set! pepsi (list (inexact->exact (round (* (random) (- BLOCKS_WIDTH 1)))) (inexact->exact (round (* (random) (- BLOCKS_HEIGHT 1)))) ))
+  (set! comida (list (inexact->exact (round (* (random) (- BLOCKS_WIDTH 1)))) (inexact->exact (round (* (random) (- BLOCKS_HEIGHT 1)))) ))
   (move-cobra direcao)
   (set! pontos (add1 pontos))
   (set! recorde (atualiza_recorde pontos))
   (set! cobra (append cobra (list x)))))
 
 ; restart: ->void
-; objetivo: seta todas as variaveis para o estado original
+; ação: seta todas as variaveis para o estado original
 (define restart (lambda()
-  (set! direcao 'd)
-  (set! pepsi (list 9 8))
+  (set! direcao 'direita)
+  (set! comida (list 9 8))
   (set! cobra (list (list 2 1) (list 1 1)))
   (set! pontos 0)
 ))
 
 ; cria a janela para a cobra
-(define frame (new frame% 
+(define frame (new frame%
   [label "Cobra"]
   [width (* BLOCKS_WIDTH BLOCK_SIZE)]
   [height (* BLOCKS_HEIGHT BLOCK_SIZE)]))
@@ -102,36 +118,38 @@
 (define (canvas-key frame) (class canvas%
   (define/override (on-char key-event)
     (cond ; todo: in_list? -> set direcao
-      [(eq? (send key-event get-key-code) 'left) (set! direcao 'l)]
-      [(eq? (send key-event get-key-code) 'right) (set! direcao 'r)]
-      [(eq? (send key-event get-key-code) 'up) (set! direcao 'u)]
-      [(eq? (send key-event get-key-code) 'down) (set! direcao 'd)]
-      [(eq? (send key-event get-key-code) '#\r) (restart)]))
+      [(eq? (send key-event get-key-code) 'left) (set! direcao 'esquerda)]
+      [(eq? (send key-event get-key-code) 'right) (set! direcao 'direita)]
+      [(eq? (send key-event get-key-code) 'up) (set! direcao 'cima)]
+      [(eq? (send key-event get-key-code) 'down) (set! direcao 'baixo)]
+      [(eq? (send key-event get-key-code) '#\i) (set! IA #t) (restart)]
+      [(eq? (send key-event get-key-code) '#\r) (set! IA #f) (restart)]))
   (super-new [parent frame])))
 
 ; atualiza-cobra: -> void
-; objetivo: funções para a atualização da tela e movimentação da cobra
+; ação: funções para a atualização da tela e movimentação da cobra
 (define atualiza-cobra (lambda ()
   (cond [IA (set! direcao (andando-sozinho))])
-  (draw-block dc (list-ref pepsi 0) (list-ref pepsi 1) "red") ; desenha a pepsi
-  (cond [(encostou-bloco cobra pepsi) (cresce-cobra)] [else (move-cobra direcao)]) ; checa por colisão com a pepsi
+  (draw-block dc (list-ref comida 0) (list-ref comida 1) "red") ; desenha a comida
+  (cond [(encostou-bloco cobra comida) (cresce-cobra)] [else (move-cobra direcao)]) ; checa por colisão com a comida
   (send dc draw-text (number->string pontos) (-(* BLOCKS_WIDTH BLOCK_SIZE) 310) 10)
   (for ([block cobra]) (
-    if (eq? block (car cobra)) 
-      (draw-block dc (list-ref block 0) (list-ref block 1) "black") 
+    if (eq? block (car cobra))
+      (draw-block dc (list-ref block 0) (list-ref block 1) "black")
       (draw-block dc (list-ref block 0) (list-ref block 1) "black")))))
 
-; lost-the-gaem: -> void
-; objetivo: eventos que ocorrem ao perder o jogo
-(define lost-the-gaem (lambda ()
-  (send dc draw-text "Fim de jogo" (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 75) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 60))
-  (send dc draw-text "Pontos:" (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 75) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 40))
-  (send dc draw-text (number->string pontos) (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) -10) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 40))
-  (send dc draw-text "Recorde:" (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 75) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 20))
-  (send dc draw-text (number->string recorde) (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) -20) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 20))                      
-  (send dc draw-text "(r) recomeçar" (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 75) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 0))                     
+; fim-de-jogo: -> void
+; ação: eventos que ocorrem ao perder o jogo
+(define fim-de-jogo (lambda ()
+  (send dc draw-text "Fim de jogo" (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 115) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 60))
+  (send dc draw-text "Pontos:" (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 115) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 40))
+  (send dc draw-text (number->string pontos) (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 30) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 40))
+  (send dc draw-text "Recorde:" (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 115) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 20))
+  (send dc draw-text (number->string recorde) (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 20) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 20))
+  (send dc draw-text "(i) recomeçar modo IA" (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 115) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) 0))
+  (send dc draw-text "(r) recomeçar modo jogador" (- (round (/ (* BLOCKS_WIDTH BLOCK_SIZE) 2)) 115) (- (round (/ (* BLOCKS_HEIGHT BLOCK_SIZE) 2)) -20))
 ))
-  
+
 ; instancia o canvas
 (define canvas (
   new (canvas-key frame)))
@@ -150,31 +168,16 @@
 (define timer (new timer%
   [notify-callback (lambda()
     (send dc clear)
-    (send dc set-brush "gray" 'solid)        
-    (send dc draw-rectangle 0 0 (* BLOCKS_WIDTH BLOCK_SIZE) (* BLOCKS_HEIGHT BLOCK_SIZE))                 
-    
+    (send dc set-brush "gray" 'solid)
+    (send dc draw-rectangle 0 0 (* BLOCKS_WIDTH BLOCK_SIZE) (* BLOCKS_HEIGHT BLOCK_SIZE))
     (define colisao #f)
     (for ([block cobra]
          [j (in-naturals 0)])
-      (cond 
+      (cond
             [(or (> (list-ref block 0) BLOCKS_WIDTH) (> 0 (list-ref block 0))) (set! colisao #t )]
             [(or (> (list-ref block 1) BLOCKS_HEIGHT) (> 0 (list-ref block 1))) (set! colisao #t)]
             [(eq? #f colisao) (set! colisao (eq? #t (encostou-bloco cobra block 0 j)))]))
-    (if colisao (lost-the-gaem) (atualiza-cobra)))]
+    (if colisao (fim-de-jogo) (atualiza-cobra)))]
   [interval #f]))
 
 (send timer start 100)
-
-;tentando
-(define (andando-sozinho)
-  (define x  (- (pega-cabeca 0 cobra) (list-ref pepsi 0)))
-  (define y  (- (pega-cabeca 1 cobra) (list-ref pepsi 1)))
-  (define abs-x (abs x))
-  (define abs-y (abs y))
-  (cond
-   [(> abs-x abs-y)
-    (if (> x 0) (if (eq? direcao 'r) 'u 'l)
-                (if (eq? direcao 'l) 'd 'r))]
-   [else
-    (if (> y 0) (if (eq? direcao 'd) 'l 'u)
-                (if (eq? direcao 'u) 'r 'd))]))
